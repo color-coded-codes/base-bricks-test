b10b68e937d02de76b5c0cb4b3ee337bf2113188
Amine Lemaizi <40530011+aminelemaizi@users.noreply.github.com>
2023-08-20 20:19:44 +0100

    wip

--- basic_operations.py
+++ basic_operations.py
@@ -1,16 +1,60 @@
 def add_two_numbers(a, b):
     """
-    # Add two numbers
+    ![Alt text](https://images.unsplash.com/photo-1508009603885-50cf7c579365?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1950&q=80)
+    # Heading 1
+    ## Heading 2
+    ### Heading 3
+    #### Heading 4
+    ##### Heading 5
+    ###### Heading 6
+    
+    Lorem **ipsum dolor** sit amet, [Google](https://www.google.com) consectetur adipiscing elit. Curabitur rhoncus, justo eget volutpat semper, erat sapien mollis elit, eget congue est augue et metus. Proin sit amet libero id eros vestibulum consequat. Vivamus posuere, elit vitae interdum sollicitudin, risus justo egestas velit, vitae dapibus odio orci vitae nunc.
+    
+    **Fusce sed enim volutpat**, Lorem *ipsum dolor sit amet*, consectetur adipiscing elit. Curabitur rhoncus, justo eget volutpat semper, erat sapien mollis elit, eget congue est augue et metus. Proin sit amet libero id eros vestibulum consequat. Vivamus posuere, elit vitae interdum sollicitudin, risus justo egestas velit, vitae dapibus odio orci vitae nunc. Fusce sed enim volutpat.
 
-    This function attempts to add two numbers provided as input. If the inputs
-    are not valid numbers, it raises a ValueError with a relevant error message.
+    Fusce sed enim volutpat:
+    - Item 1
+    - Item 2
 
-    ## Args:
-    - **a**: First input value.
-    - **b**: Second input value.
+    ---
+    Fusce sed enim volutpat:
+    1. First item
+    2. Second item
 
-    ## Returns:
-    Sum of the two input values.
+    > This is a blockquote
+
+    `requirements==1.2`
+
+    And a code blpck here:
+
+    ```
+    from django.conf import settings
+
+    for x in y:
+        print(x)
+    
+    
+    ```
+    ---
+
+    For each thing there is a ~~strikethrough~~ to show!
+
+    | Header 1 | Header 2 |
+    |----------|----------|
+    | Cell 1   | Cell 2   |
+    | Cell 3   | Cell 4   |
+
+    - [x] Task 1 (completed)
+    - [ ] Task 2 (not completed)
+
+    is this emoji working?? :smile:
+
+    Term 1
+    : Definition 1
+
+    Term 2
+    : Definition 2
+
     """
     error_message = None
     result = None
